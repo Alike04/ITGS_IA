b994c8b5a8123a9b17c64bddff471b8a8e4e4ada
Alike04 <shamenov2004@gmail.com>
2023-03-09 21:13:52 +0600

    link fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ITGS_IA",
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/ITGS_IA/",
+});
